a6f5d186b9182da556bf74e7bd45f06432f27492
prince-0303 <princebinil2003@gmail.com>
2025-10-31 15:11:32 +0530

    added students app and templates

--- student_lms/settings.py
+++ student_lms/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'students',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+TEMPLATES[0]['DIRS'] = [BASE_DIR / "templates"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
