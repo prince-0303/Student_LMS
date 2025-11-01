70f578b945d4ffc3d9a9f148385d4cf08938d9d1
prince-0303 <princebinil2003@gmail.com>
2025-11-01 12:09:18 +0530

    Added login log out view

--- students/views.py
+++ students/views.py
@@ -1,4 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import StudentRegistrationForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home.html')
+
+def register_view(request) :
+    if request.method == 'POST':
+        form = StudentRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.role = 'Student'
+            user.save()
+            messages.success(request, 'Registration successful! You can log in.')
+            return redirect('login')
+    else:
+        form = StudentRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request) : 
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"Welcome back, {user.username}!")
+            if user.role == 'Admin':
+                return redirect('admin_dashboard')
+            else:
+                return redirect('student_dashboard')
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'login.html')
+
+def logout_view(request) :
+    logout(request)
+    messages.info(request, 'You have been logged out.')
+    return redirect('login')
